b98587ab687f9168c0a8d998733527c7212211e3
TheBlackCode2 <yassinew20w@gmail.com>
2024-01-10 13:02:43 +0100

    new update

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    copy_list = my_list.copy()
+    for i in range(0, len(copy_list)):
+        if (copy_list[i] == search):
+            copy_list[i] = replace
+    return copy_list
